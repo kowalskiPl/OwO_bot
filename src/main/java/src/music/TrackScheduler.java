7b06c14c81e0fa403a5d807a3d91fbfa3db08f58
Patryk Kobus <patryk.maciej.kobus@gmail.com>
2022-07-19 00:21:55 +0200

    Some initial changes

--- src/main/java/src/music/TrackScheduler.java
+++ src/main/java/src/music/TrackScheduler.java
@@ -6,6 +6,8 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import net.dv8tion.jda.api.entities.Channel;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.internal.utils.tuple.ImmutablePair;
+import net.dv8tion.jda.internal.utils.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +17,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class TrackScheduler extends AudioEventAdapter {
     private final AudioPlayer player;
-    private final BlockingQueue<AudioTrack> queue;
+    private final BlockingQueue<Pair<MessageChannel, AudioTrack>> queue;
     private MessageChannel linkedTextChannel;
 
     private static final Logger log = LoggerFactory.getLogger(TrackScheduler.class);
@@ -35,7 +37,7 @@ public class TrackScheduler extends AudioEventAdapter {
 
     public void enqueue(AudioTrack track) {
         if (!player.startTrack(track, true)) {// check if the player is free and play the track if so, else enqueue
-            queue.offer(track);
+            queue.offer(new ImmutablePair<>(linkedTextChannel, track));
             linkedTextChannel.sendMessage("Added: " + track.getInfo().title + " to queue").queue();
         } else
             linkedTextChannel.sendMessage("Now playing: " + track.getInfo().title).queue();
@@ -44,8 +46,8 @@ public class TrackScheduler extends AudioEventAdapter {
     public void nextTrack() {
         var track = queue.poll();
         if (track != null) {
-            player.startTrack(track, false);
-            linkedTextChannel.sendMessage("Now playing: " + track.getInfo().title).queue();
+            player.startTrack(track.getRight(), false);
+            track.getLeft().sendMessage("Now playing: " + track.getRight().getInfo().title).queue();
         }
 
     }
