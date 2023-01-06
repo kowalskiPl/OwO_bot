a3c804598585d4cfda8b2a8f8c57722f695ea61f
Patryk Kobus <patryk.maciej.kobus@gmail.com>
2023-01-06 07:02:04 +0100

    some initial changes

---
+++ src/main/java/com/owobot/middleware/Middleware.java
@@ -0,0 +1,18 @@
+package com.owobot.middleware;
+
+import com.owobot.OwoBot;
+import com.owobot.commands.Command;
+import com.owobot.utilities.Reflectional;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class Middleware extends Reflectional {
+
+    public Middleware(OwoBot owoBot) {
+        super(owoBot);
+    }
+    @Nullable
+    public String buildHelp(Command command){
+        return null;
+    }
+    public abstract boolean handle(Command command);
+}
